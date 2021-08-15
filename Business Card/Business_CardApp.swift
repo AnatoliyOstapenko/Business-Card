8a0984d3ba7f4714f4add2b558cf635cc54b1c3f
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-08-15 19:18:46 +0300

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by MacBook on 15.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
